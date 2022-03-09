ec85c711208e44bae6aed528a8c321162824e045
Jorge <jcintado@alintia.com>
2022-03-09 12:58:56 +0100

    More cases.

---
+++ basic/directives.cpp
@@ -0,0 +1,25 @@
+#include<iostream> 
+#define val 12
+// macro with parameter 
+#define AREA(l) (l * l)
+
+#if defined dimen
+    // This will be compiled
+    // if 'dimen'  i s  defined
+#else
+    // This will be compiled if
+    // 'dimen' is  n o t  defined
+#endif
+
+using namespace std;
+
+int main()
+{
+    cout << "Value is :" << val << endl;
+
+    int x = 10, area; 
+    area = AREA(x);
+    cout << "Area of square is: " << area;
+
+    return 0;
+}
