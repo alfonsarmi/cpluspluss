081f6a58f44f4c548cb684f3df20b40bf1640328
Julio Gutierrez <jgutierrez@foreworth>
2022-04-26 13:26:05 +0200

    More loops cases for ECSL tests

--- basic/loops.cpp
+++ basic/loops.cpp
@@ -4,21 +4,38 @@ using namespace std;
 int main() {
     //while:
     int i = 0;
+    int j = 0;
     do {
-        cout << i << "\n";
+        cout << i << "\n";        
+        for (int j = 0; j <= 10; j = j + 2) {
+            cout << i << " ";
+            if (i == 4) {
+                break;
+            }
+            else {
+                continue;
+            }
+            cout << i << "\n";
+        }
         i++;
     }
     while (i < 5);
 
     //for
-    for (int i = 0; i <= 10; i = i + 2) {
-        cout << i << " ";
-        if (i == 4) {
+    int i2 = 0;
+    int j2 = 0;
+    for (int i2 = 0; i2 <= 10; i2 = i2 + 2) {
+        cout << i2 << " ";
+        do {
+            cout << i2 << "\n";
+            j2++;
+        } while (j2 < 5);
+        if (i2 == 4) {
             break;
         } else {
             continue;            
         }        
-        cout << i << "\n";
+        cout << i2 << "\n";
     }
 
 
