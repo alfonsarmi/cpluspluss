bdf5083e9ad09ec25797cccde62917adc488b95f
Jose Antonio Gutierrez <jagutierrez@foreworth.com>
2022-05-20 16:06:12 +0200

    Halstead - class with operators without methods

---
+++ prod_5.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace test;
+
+class t_class {
+  public:
+    int myNum = 5;               // Integer (whole number without decimals)
+    double myFloatNum = 5.99;    // Floating point number (with decimals)
+    char myLetter = 'D';         // Character
+    string myText = "Hello";     // String (text)
+    bool myBoolean = true;       // Boolean (true or false)
+};
