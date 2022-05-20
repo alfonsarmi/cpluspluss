3cc70c355dc7539c1ea5742505ef140aebbdf59e
Jose Antonio Gutierrez <jagutierrez@foreworth.com>
2022-05-20 13:03:33 +0200

    Renaming a class and add a new class

--- prod_4.cpp
+++ prod_4.cpp
@@ -1,5 +1,13 @@
+// Create a Truck class with some attributes
+class Camion {
+  public:
+    string brand;   
+    string model;
+    int year;
+};
+
 // Create a Car class with some attributes
-class Coche {
+class Cochazo {
   public:
     string brand;   
     string model;
@@ -8,13 +16,13 @@ class Coche {
 
 int main() {
   // Create an object of Car
-  Coche carObj1;
+  Cochazo carObj1;
   carObj1.brand = "BMW";
   carObj1.model = "X5";
   carObj1.year = 1999;
 
   // Create another object of Car
-  Coche carObj2;
+  Cochazo carObj2;
   carObj2.brand = "Ford";
   carObj2.model = "Mustang";
   carObj2.year = 1969;
