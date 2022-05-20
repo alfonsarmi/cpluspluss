2318c66842a4d8136c5f297931c15efa703afaff
Jose Antonio Gutierrez <jagutierrez@foreworth.com>
2022-05-20 13:01:48 +0200

    Renaming a class - Car -> Coche

--- prod_4.cpp
+++ prod_4.cpp
@@ -1,5 +1,5 @@
 // Create a Car class with some attributes
-class Car {
+class Coche {
   public:
     string brand;   
     string model;
@@ -8,13 +8,13 @@ class Car {
 
 int main() {
   // Create an object of Car
-  Car carObj1;
+  Coche carObj1;
   carObj1.brand = "BMW";
   carObj1.model = "X5";
   carObj1.year = 1999;
 
   // Create another object of Car
-  Car carObj2;
+  Coche carObj2;
   carObj2.brand = "Ford";
   carObj2.model = "Mustang";
   carObj2.year = 1969;
