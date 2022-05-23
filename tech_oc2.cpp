5a0150d1651e91390627f9f704ae06529602eb95
Jose Antonio Gutierrez <jagutierrez@foreworth.com>
2022-05-23 13:13:28 +0200

    Cognitive complexity increases > 30

--- tech_oc2.cpp
+++ tech_oc2.cpp
@@ -3,12 +3,24 @@ using namespace std;
 
 int main() {
   int time = 22;
-  if (time < 10) {
-    cout << "Good morning.";
-  } else if (time < 20) {
-    cout << "Good day.";
-  } else {
-    cout << "Good evening.";
+  if (time < 1) {
+    cout << "Good 1.";
+  } else if (time < 2) {
+    cout << "Good 2.";
+  } else if (time < 3) {
+    cout << "Good 3.";
+  } else if (time < 4) {
+    cout << "Good 4.";
+  } else if (time < 5) {
+    cout << "Good 5.";
+  } else if (time < 6) {
+    cout << "Good 6.";
+  } else if (time < 7) {
+    cout << "Good 7.";
+  } else if (time < 8) {
+    cout << "Good 8.";
+  } else if (time < 9) {
+    cout << "Good 9.";
   }
   return 0;
 }
